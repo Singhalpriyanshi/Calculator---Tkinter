133cf2ea92bed42a3acf2bf032ba2aa9c49c780a
Singhalpriyanshi <89973163+Singhalpriyanshi@users.noreply.github.com>
2023-08-11 16:58:04 +0530

    Add files via upload

---
+++ 1st code- grid.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+root = Tk()
+
+#Creating a label widget
+myLabel1 = Label(root, text = "Hello World!").grid(row = 0, column = 0)
+myLabel2 = Label(root, text = "My name is Priyanshi Singhal").grid(row = 1, column = 5)
+#Shoving it onto the screen
+
+
+root.mainloop()
+
